389351887959b266106d3042f1739d6bc4080a62
al-c95 <137837298+al-c95@users.noreply.github.com>
2022-01-01 22:04:16 +1100

    first pass at menu bar

--- src/TextFrame.cpp
+++ src/TextFrame.cpp
@@ -1,8 +1,19 @@
 #include "TextFrame.h"
 
+const int ID_FILE_EXIT = 1000;
+
 TextFrame::TextFrame()
     : wxFrame(NULL, -1, "wxPad")
 {
     text = new wxTextCtrl(this, -1, "Type some text here!",
                           wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
+
+    // initialise menu
+    wxMenu *menu = new wxMenu();
+    menu->Append(ID_FILE_EXIT, "Exit");
+
+    // add menu to menubar
+    wxMenuBar *menu_bar = new wxMenuBar();
+    SetMenuBar(menu_bar);
+    menu_bar->Append(menu, "File");
 }
