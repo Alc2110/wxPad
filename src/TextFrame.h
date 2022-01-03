aeaf36103a572af4e66300869e7b7065121559cd
al-c95 <137837298+al-c95@users.noreply.github.com>
2022-01-03 21:28:48 +1100

    make event handlers private

--- src/TextFrame.h
+++ src/TextFrame.h
@@ -7,6 +7,12 @@ class TextFrame : public wxFrame
 public:
     TextFrame();
 
+private:
+    wxTextCtrl *text_area;
+    std::string current_file_path;
+
+    void save_as();
+
     void OnExit(wxCommandEvent &event);
     void OnSave(wxCommandEvent &event);
     void OnSaveAs(wxCommandEvent &event);
@@ -15,12 +21,6 @@ public:
     void OnReplace(wxCommandEvent &event);
     void OnSelectAll(wxCommandEvent &event);
     void OnFormatFont(wxCommandEvent &event);
-
-private:
-    wxTextCtrl *text_area;
-    std::string current_file_path;
-
-    void save_as();
 
     DECLARE_EVENT_TABLE();
 };
